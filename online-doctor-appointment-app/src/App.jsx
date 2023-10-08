c9dfa57ab880edf505eb8b69e56d74fe742c0f4c
Shaikh Haris Ali <shaikhharisali16@gmail.com>
2023-10-09 01:41:17 +0500

    Setup React Router

--- online-doctor-appointment-app/src/App.jsx
+++ online-doctor-appointment-app/src/App.jsx
@@ -1,15 +1,41 @@
 
-import './App.css'
+import {BrowserRouter,Routes,Route,Link} from 'react-router-dom'
+import Home from './pages/HomePage.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
+
 
 function App() {
   
 
   return (
     <>
-   
-    <div className="bg-dark">
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+    </Routes>
+    {/* <div className="bg-dark">
     <h1 className='text-success'>Hello world </h1> 
-    </div>
+    </div> */}
+     <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+   
+    </BrowserRouter>
+    
+    
     </>
   )
 }
